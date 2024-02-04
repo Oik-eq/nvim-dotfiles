ce4f15a0517d40c5f78c9811a461ebf1db24dc92
Oik <lucas.dimartino.c@gmail.com>
2024-02-05 00:08:41 +0100

    adding rust support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" }
+                ensure_installed = { "lua_ls", "pyright", "rust_analyzer" }
             })
         end
     },
@@ -22,6 +22,9 @@ return {
                 capabilities = capabilities
             })
             lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
+            lspconfig.rust_analyzer.setup({
                 capabilities = capabilities
             })
             lspconfig.ltex.setup({
